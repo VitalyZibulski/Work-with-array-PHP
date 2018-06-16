0c9931f545da331258c739276ac0f1821853c9fe
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-06-17 01:42:11 +0300

    add counting and looping for arrays

---
+++ 1.Counting/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$people = ['a', 'b', 'c'];
+
+//echo count($people);
+
+$count = array_count_values($people);
+
+//var_dump($count);
+
+//array(3) { ["a"]=> int(1) ["b"]=> int(1) ["c"]=> int(1) }
+
+//echo count($people);
+
+//3
+
+//echo sizeof($people);
+
+//3
+
+
---
+++ 2.Looping/index.php
@@ -0,0 +1,97 @@
+
+
+/*$statuses = [
+
+	['text' => 'one text'],
+	['text' => 'two text'],
+	'status' => 200,
+];
+
+
+foreach($statuses as $index => $status){
+	//var_dump($statuses);
+	if($index !== 'status'){// or if($index === 'status'){continue;}
+		echo $status['text'], '<br>';
+	}
+
+}*/
+
+$people = [
+
+	[
+		'id' => 1,
+		'name' => 'antony',
+		'email' => 'antony@mail.com',
+
+	],
+
+	[
+		'id' => 2,
+		'name' => 'ron',
+		'email' => 'ron@mail.com',
+
+	],
+
+
+
+];
+
+
+/*$toFind = 'antony';
+
+$found = false;
+
+
+foreach($people as $person){
+
+	if($person['name'] === $toFind){
+		$found = $person;
+		break;
+
+	}
+
+}
+
+var_dump($found);*/
+
+foreach($people as &$person){
+
+		$person['name'] = 'dale';
+
+}
+
+var_dump($people);
+
+
+//or
+
+
+$newPeople = [];
+
+foreach($people as $person){
+	$person['name'] = 'dale';
+	$newPeople = $person;
+
+}
+
+var_dump($people);
+
+
+
+--------------------------------------------------------------
+
+$numbers = [1,2,3,4];
+
+foreach($numbers as &$number){
+	$number = $number * 2;
+	unset($number);
+}
+
+foreach($numbers as $number){
+	var_dump($number);
+}
+
+
+
+
+
