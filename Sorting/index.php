b09a3199395677a39333fd6fa05379457dba7789
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-06-20 13:29:54 +0300

    add sorting

---
+++ Sorting/index.php
@@ -0,0 +1,37 @@
+<?php
+
+	/*sort();
+
+	rsort();
+
+
+	asort();
+
+	arsort();
+
+
+	ksort();
+
+
+	krsort();
+
+
+	natsort();
+
+
+	usort();
+
+	uksort();
+
+	array_multisort();*/
+
+
+
+	$names = ['antony', 'antony1', 'ron', 'ron1'];
+
+
+//sort($names); // as well numbers
+
+	rsort($names);
+
+	var_dump($names);
