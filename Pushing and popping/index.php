98ee723b4b44c8552e926187b097acc32eff16cb
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-06-20 14:06:00 +0300

    add push and pop and pop

---
+++ Pushing and popping/index.php
@@ -0,0 +1,16 @@
+<?php
+
+	$users = ['antony', 'ron'];
+
+
+//$users[] = 'antony1';
+
+//array_push($users, 'antony1', 'ron1');
+
+//var_dump($users);
+
+
+	array_pop($users);
+
+
+	var_dump($users);
